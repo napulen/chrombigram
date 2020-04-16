13c0189a40502a3476c1a1dbc9b53784a5cacf0a
Nestor Napoles Lopez <napulen@gmail.com>
2020-04-16 10:29:23 -0400

    Supporting midi-only for now

--- chrombigram/__main__.py
+++ chrombigram/__main__.py
@@ -1,25 +1,14 @@
 from chrombigram.chrombigram import Chrombigram, ChrombigramCounter
-import music21
+from chrombigram.utilities import getpcs_midi
 
 import sys
 import pprint
 
-def getpcs_music21(m21):
-    allpcs = []
-    for x in m21.chordify().flat.notes:
-        pcs = []
-        for n in x:
-            pcs.append(n.pitch.pitchClass)
-        pcs = frozenset(pcs)
-        allpcs.append(pcs)
-    return allpcs
-
 if __name__ == '__main__':
-    score = music21.converter.parse(sys.argv[1])
-    scorepcs = getpcs_music21(score)
+    pcss = getpcs_midi(sys.argv[1])
     counter = ChrombigramCounter()
-    for pc in scorepcs:
-        chrombi = Chrombigram(pc)
+    for pcs in pcss:
+        chrombi = Chrombigram(pcs)
         counter.append(chrombi)
-        # print(chrombi, end=' ')
-    pprint.pprint(counter.chrombigram_dict)
+        print(chrombi, end=' ')
+    # pprint.pprint(counter.chrombigram_dict)
--- chrombigram/__version__.py
+++ chrombigram/__version__.py
@@ -1,5 +1,5 @@
 
 
-VERSION = (0, 3, 0)
+VERSION = (0, 4, 0)
 
 __version__ = '.'.join(map(str, VERSION))
